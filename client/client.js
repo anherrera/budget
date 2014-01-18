9da4f0e23eb1b95918179924d35e3c28def5d8b1
Alexa Herrera <alexa.herrera90@gmail.com>
2014-01-18 14:55:41 -0800

    #6: Require log in before using app, or allow temporary anon session

--- client/client.js
+++ client/client.js
@@ -1,25 +1,25 @@
 /**
- * This work is licensed under the Creative Commons Attribution-NonCommercial 
- * 4.0 International License. To view a copy of this license, visit 
- * http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial
+ * 4.0 International License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
  * Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
  */
 
-Handlebars.registerHelper('balance', function() {
-        return Session.get('balance');
+Handlebars.registerHelper('balance', function () {
+    return Session.get('balance');
 });
-Handlebars.registerHelper('start', function() {
-        return Session.get('start');
+Handlebars.registerHelper('start', function () {
+    return Session.get('start');
 });
-Handlebars.registerHelper('end', function() {
-        return Session.get('end');
+Handlebars.registerHelper('end', function () {
+    return Session.get('end');
+});
+
+Handlebars.registerHelper('isLoggedIn', function() {
+    return Meteor.userId() !== null;
 });
 
 Meteor.subscribe('events');
-
-Meteor.startup(function() {
-
-});
 
 /* pass in moment objects */
 function getEvents(start, end) {
@@ -34,7 +34,7 @@ function getEvents(start, end) {
         end = moment().add('month', 1);
     }
 
-    $.each(events, function(idx, e) {
+    $.each(events, function (idx, e) {
 
         var currDate = e.date;
 
@@ -65,7 +65,7 @@ function getEvents(start, end) {
         }
     });
 
-    eventList.sort(function(a,b) {
+    eventList.sort(function (a, b) {
         if (a.date == b.date) {
             return a.type == 'income' ? -1 : (a.type == b.type) ? 0 : 1;
         }
@@ -81,126 +81,126 @@ function getEvents(start, end) {
 }
 
 Template.eventsTable.calendarEvents = function () {
-        var start = moment().hour(0).minute(0).second(0);
-        var end = moment().add('month', 1);
+    var start = moment().hour(0).minute(0).second(0);
+    var end = moment().add('month', 1);
 
-        if (Session.get('start')) {
-                start = moment(Session.get('start'));
-        }
-        if (Session.get('end')) {
-                end = moment(Session.get('end'));
-        }
+    if (Session.get('start')) {
+        start = moment(Session.get('start'));
+    }
+    if (Session.get('end')) {
+        end = moment(Session.get('end'));
+    }
 
-        return getEvents(start, end);
+    return getEvents(start, end);
 };
 
 Template.addEvent.events = {
-        'click .add-event': function () {
-                $('#add-event-modal').modal('show');
-        },
-        'click .save-event': function (e) {
-                e.preventDefault();
-                var data = $('#add-event-form').serializeArray();
+    'click .add-event': function () {
+        $('#add-event-modal').modal('show');
+    },
+    'click .save-event': function (e) {
+        e.preventDefault();
+        var data = $('#add-event-form').serializeArray();
 
-                var newEvent = {};
-                $.each(data, function (idx, elem) {
-                        newEvent[elem.name] = elem.value;
-                });
+        var newEvent = {};
+        $.each(data, function (idx, elem) {
+            newEvent[elem.name] = elem.value;
+        });
 
-                newEvent.amount = parseFloat(newEvent.amount);
+        newEvent.amount = parseFloat(newEvent.amount);
 
-                if (newEvent._id != "") {
-                    Events.update(newEvent._id, {
-                        name: newEvent.name,
-                        type: newEvent.type,
-                        amount: newEvent.amount,
-                        date: moment(newEvent.date).format('YYYY-MM-DD'),
-                        recurringInterval: newEvent.recurringInterval,
-                        recurringCount: newEvent.recurringCount,
-                        userId: Meteor.userId()
-                    });
-                } else {
-                    Events.insert({
-                        name: newEvent.name,
-                        type: newEvent.type,
-                        amount: newEvent.amount,
-                        date: moment(newEvent.date).format('YYYY-MM-DD'),
-                        recurringInterval: newEvent.recurringInterval,
-                        recurringCount: newEvent.recurringCount,
-                        userId: Meteor.userId()
-                    });
-                }
-
-                $('#add-event-form').find('input, select').not('[type=submit]').val('');
-                $('#add-event-modal').modal('hide');
-        },
-        'change #recurring': function() {
-                if ($('#recurring').is(':checked')) {
-                        $('#recurring_fields').find('input,select').removeAttr('disabled');
-                } else {
-                        $('#recurring_fields').find('input,select').attr('disabled', 'disabled');
-                }
+        if (newEvent._id != "") {
+            Events.update(newEvent._id, {
+                name: newEvent.name,
+                type: newEvent.type,
+                amount: newEvent.amount,
+                date: moment(newEvent.date).format('YYYY-MM-DD'),
+                recurringInterval: newEvent.recurringInterval,
+                recurringCount: newEvent.recurringCount,
+                userId: Meteor.userId()
+            });
+        } else {
+            Events.insert({
+                name: newEvent.name,
+                type: newEvent.type,
+                amount: newEvent.amount,
+                date: moment(newEvent.date).format('YYYY-MM-DD'),
+                recurringInterval: newEvent.recurringInterval,
+                recurringCount: newEvent.recurringCount,
+                userId: Meteor.userId()
+            });
         }
+
+        $('#add-event-form').find('input, select').not('[type=submit]').val('');
+        $('#add-event-modal').modal('hide');
+    },
+    'change #recurring': function () {
+        if ($('#recurring').is(':checked')) {
+            $('#recurring_fields').find('input,select').removeAttr('disabled');
+        } else {
+            $('#recurring_fields').find('input,select').attr('disabled', 'disabled');
+        }
+    }
 };
 
 Template.eventsTable.events = {
-        'click .delete': function () {
-                if (confirm('Are you sure you want to delete this?')) {
-                        Events.remove(this._id);
-                }
-        },
-        'click .edit': function () {
-                var f = $('#add-event-form');
-                f.find('[name=_id]').val(this._id);
-                f.find('[name=name]').val(this.name);
-                f.find('[name=type]').val(this.type);
-                f.find('[name=date]').val(moment(this.date).format('MM/DD/YYYY'));
-                f.find('[name=amount]').val(this.amount);
-
-                if (this.recurringInterval && this.recurringCount) {
-                        f.find('#recurring').attr('checked', true);
-                        f.find('#recurring_fields').find('input,select').removeAttr('disabled');
-                }
-
-                f.find('[name=recurringCount]').val(this.recurringCount);
-                f.find('[name=recurringInterval]').val(this.recurringInterval);
-                $('#add-event-modal').modal('show');
+    'click .delete': function () {
+        if (confirm('Are you sure you want to delete this?')) {
+            Events.remove(this._id);
         }
+    },
+    'click .edit': function () {
+        var f = $('#add-event-form');
+        f.find('[name=_id]').val(this._id);
+        f.find('[name=name]').val(this.name);
+        f.find('[name=type]').val(this.type);
+        f.find('[name=date]').val(moment(this.date).format('MM/DD/YYYY'));
+        f.find('[name=amount]').val(this.amount);
+
+        if (this.recurringInterval && this.recurringCount) {
+            f.find('#recurring').attr('checked', true);
+            f.find('#recurring_fields').find('input,select').removeAttr('disabled');
+        }
+
+        f.find('[name=recurringCount]').val(this.recurringCount);
+        f.find('[name=recurringInterval]').val(this.recurringInterval);
+        $('#add-event-modal').modal('show');
+    }
 };
 
 Template.snapshot.totalIncome = function () {
-        var totalIncome = Session.get('balance') ? Session.get('balance') : 0;
-        var events = getEvents();
+    var totalIncome = Session.get('balance') ? Session.get('balance') : 0;
+    var events = getEvents();
 
-        $.each(events, function(idx, e) {
-                if (e.type =='income') {
-                        totalIncome += parseFloat(e.amount);
-                }
-        });
+    $.each(events, function (idx, e) {
+        if (e.type == 'income') {
+            totalIncome += parseFloat(e.amount);
+        }
+    });
 
-        return totalIncome.toFixed(2);
+    return totalIncome.toFixed(2);
 };
 
-Template.snapshot.totalExpenses = function() {
-        var totalExpenses = 0;
-        var events = getEvents();
+Template.snapshot.totalExpenses = function () {
+    var totalExpenses = 0;
+    var events = getEvents();
 
-        $.each(events, function(idx, e) {
-                if (e.type =='bill') {
-                        totalExpenses += parseFloat(e.amount);
-                }
-        });
+    $.each(events, function (idx, e) {
+        if (e.type == 'bill') {
+            totalExpenses += parseFloat(e.amount);
+        }
+    });
 
-        return totalExpenses.toFixed(2);
+    return totalExpenses.toFixed(2);
 };
 
-Template.snapshot.difference = function() {
-        var difference = parseFloat(Template.snapshot.totalIncome()) - parseFloat(Template.snapshot.totalExpenses());
-        return difference.toFixed(2);
+Template.snapshot.difference = function () {
+    var difference = parseFloat(Template.snapshot.totalIncome()) - parseFloat(Template.snapshot.totalExpenses());
+    return difference.toFixed(2);
 };
 
 Template.snapshot.events = {
-        'blur #balance': function() {
-                Session.set('balance', parseFloat($('#balance').val()));
-        }
+    'blur #balance': function () {
+        Session.set('balance', parseFloat($('#balance').val()));
+    }
 };
