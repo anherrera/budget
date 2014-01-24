b75e63894007908d2388ffe98ce36a5ff2481270
Alexa <alexa.herrera90@gmail.com>
2014-01-23 21:23:13 -0800

    #4 Allow drop down of show budget for...

--- client/js/init.js
+++ client/js/init.js
@@ -21,5 +21,17 @@ $().ready(function() {
             }
         });
 
+    $('#interval').change(function() {
+        var interval = $(this).val().split(' ');
+        var amount = interval[0];
+        var time = interval[1];
+
+        var start = moment().hour(0).minute(0).second(0).format('MM/DD/YYYY');
+        var end = moment().add(time, amount).format('MM/DD/YYYY');
+
+        $('#start').val(start);
+        $('#end').val(end).triggerHandler('changeDate');
+    });
+
 	//$('#add-event-form').validate();
 });
